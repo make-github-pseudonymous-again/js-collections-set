0bb2a22a02acd598a0200c4eadc1ae2f2ccec88d
Aurélien Ooms <aurelien.ooms@gmail.com>
2020-04-28 21:29:29 +0200

    :bug: fix: Fix derivation from native Set.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import _Set from './_Set' ;
 import _set from './_set' ;
+import NativeSet from './NativeSet' ;  // Use native implementation of set
 
-const NativeSet = Set ; // Use native implementation of set
-
-Set = _Set( NativeSet ) ; // overwrite native implementation
+const Set = _Set( NativeSet ) ; // overwrite native implementation
 
 const set = _set( Set ) ;
 
